7f72be1e3aefe9ddc94ae084e63a872b2e19abce
vm0077 <chanvicheayou136@gmail.com>
2024-11-01 22:42:25 +0700

    Add animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,27 +11,46 @@ public class GameManager : MonoBehaviour {
   Image imgYou; // your selected image (rock, paper scissor)
   Image imgCom; // computer selected image (rock, paper scissor)
 
+  // animation Object
+  Animator anim_You; // your selected image (rock, paper scissor)
+  Animator anim_Com; // computer selected image (rock, paper scissor)
+
+
+
   Text txtYou;    // the score you win
   Text txtCom;    // the score computer win
   Text txtResult; // the result
   TextMeshProUGUI txtRound;
+  // entire UI except menu button
   GameObject ui_panel;
+
+  // Group of Rock, Paper and Scissor Button so it can to turn off/on in group
+  GameObject buttonGroup;
+  // game state (NOTE: this if there are something to add in future)
   GameState state;
   int round = 1;
 
   int cntYou = 0; // your scores
   int cntCom = 0; // computer's scores
+
+  int comChoice = 0;
+  int yourChoice = 0;
+
+  // Game State for different situation
   enum GameState {
     STARTING,
     CHOOSING,
+    ACTION,
     RESULT,
   };
 
 
+  // if the result between the player and computer is draw
   bool isDraw = false;
+
+  // start the first frame
   void Start() {
-    // init the game
-    InitGame();
+      InitGame();
   }
 
   // Update is called once per frame
@@ -42,44 +61,55 @@ public class GameManager : MonoBehaviour {
     }
   }
 
-  void CheckResult(int yourResult) {
+
+  void CheckResult() {
     // algorithm determine the result
     isDraw = false;
-    int comResult = UnityEngine.Random.Range(1, 4);
-    int k = yourResult - comResult;
+    comChoice = UnityEngine.Random.Range(1, 4);
+    int k = yourChoice - comChoice;
     switch (k) {
-    case 0:
-      isDraw = true;
-      txtResult.text = "Draw!!!";
-      break;
-    case 1:
-    case -2:
-      cntYou++;
-      round++;
-      txtResult.text = "You win.";
-      break;
-    default:
-      cntCom++;
-      round++;
-      txtResult.text = "Computer win.";
-      break;
+         case 0:
+           isDraw = true;
+           txtResult.text = "Draw!!!";
+           break;
+         case 1:
+         case -2:
+           cntYou++;
+           round++;
+           txtResult.text = "You win.";
+           break;
+         default:
+           cntCom++;
+           round++;
+           txtResult.text = "Computer win.";
+           break;
     }
-    SetResult(yourResult, comResult); // set game result to UI
+    //SetResult(yourResult, comResult); // set game result to UI
+    RunChoice();
   }
 
-  void SetResult(int you, int com) {
+  void RunChoice()
+  {
+        // turn to rock
+        // just like do irl
+        // invert image com in x axis
+        imgCom.transform.localScale = new Vector3(-1, 1, 1);
+        imgYou.sprite = Resources.Load("img_" + 1, typeof(Sprite)) as Sprite;
+        imgCom.sprite = Resources.Load("img_" + 1, typeof(Sprite)) as Sprite;
+        anim_Com.SetTrigger("TrActive");
+        anim_You.SetTrigger("TrActive");
+        StartCoroutine(DelayAction(0.5f, SetResult));
+  }
+
+  void SetResult() {
     // change image
     state = GameState.RESULT;
-    imgYou.sprite = Resources.Load("img_" + you, typeof(Sprite)) as Sprite;
-    imgCom.sprite = Resources.Load("img_" + com, typeof(Sprite)) as Sprite;
+    imgYou.GetComponent<Image>().sprite = Resources.Load("img_" + yourChoice, typeof(Sprite)) as Sprite;
+    imgCom.GetComponent<Image>().sprite = Resources.Load("img_" + comChoice, typeof(Sprite)) as Sprite;
 
-    // invert image com in x axis
-    imgCom.transform.localScale = new Vector3(-1, 1, 1);
     // winning score
     txtYou.text = cntYou.ToString();
     txtCom.text = cntCom.ToString();
-    if(cntYou >=3) SceneManager.LoadScene("YouWinScene");
-    if(cntCom >=3) SceneManager.LoadScene("YouLoseScene");
     txtRound.text = "Round " + round.ToString();
 
 
@@ -87,47 +117,51 @@ public class GameManager : MonoBehaviour {
         state = GameState.CHOOSING;
         return;
     }
-    // delay for 3 seconds
+    // turn off buttons
+    buttonGroup.SetActive(false);
+    // delay for 3 seconds to create the effect of changing round
     // StartRound();
     StartCoroutine(DelayAction(2, StartRound));
   }
 
-  public void OnButtonClick(GameObject buttonObject) {
-    // event when button is clicked
-    if(state == GameState.STARTING || state == GameState.RESULT) return;
-    int you = int.Parse(buttonObject.name.Substring(0, 1));
-    CheckResult(you);
-    // Debug.Log("clicked");
-  }
-  public void OnMouseExit(GameObject buttonObject) {
-    // event when mouse exit the button
 
-    // get the animator component from the button
-    Animator anim = buttonObject.GetComponent<Animator>();
-    // switch to idle animation
-    anim.Play("Normal");
-    // Debug.Log("Mouse Exit");
-  }
   private void InitGame() {
     imgYou = GameObject.Find("ImgYou").GetComponent<Image>();
     imgCom = GameObject.Find("ImgCom").GetComponent<Image>();
-
+    anim_You = GameObject.Find("ImgYou").GetComponent<Animator>();
+    anim_Com = GameObject.Find("ImgCom").GetComponent<Animator>();
     txtYou = GameObject.Find("TxtYou").GetComponent<Text>();
     txtCom = GameObject.Find("TxtCom").GetComponent<Text>();
     txtResult = GameObject.Find("TxtResult_").GetComponent<Text>();
     txtRound = GameObject.Find("TxtRound").GetComponent<TextMeshProUGUI>();
+    buttonGroup = GameObject.Find("buttonGroup");
 
     // init the text before the game start
     txtResult.text = "Select the button below";
     txtRound.text = "Round 1";
     ui_panel = GameObject.Find("UI");
+    //begin the round
     StartRound();
   }
 
   void StartRound() {
-    state = GameState.STARTING;
-    imgYou.sprite = Resources.Load("img_" + 4, typeof(Sprite)) as Sprite;
-    imgCom.sprite = Resources.Load("img_" + 4, typeof(Sprite)) as Sprite;
+
+    buttonGroup.SetActive(true);
+    if(cntYou >=3) {
+        SceneManager.LoadScene("YouWinScene");
+        return;
+    }
+    else if (cntCom >=3) {
+        SceneManager.LoadScene("YouLoseScene"); state = GameState.STARTING;
+        return;
+    }
+    yourChoice = 0;
+    comChoice = 0;
+    // return the question mark state
+    imgYou.sprite  = Resources.Load("img_" + 4, typeof(Sprite)) as Sprite;
+    imgCom.sprite= Resources.Load("img_" + 4, typeof(Sprite)) as Sprite;
+    // rotate
+    imgCom.transform.localScale = new Vector3(1, 1, 1);
     txtRound.gameObject.SetActive(true);
     ui_panel.SetActive(false);
 
@@ -140,13 +174,28 @@ public class GameManager : MonoBehaviour {
     }));
   }
 
-
+// Delay function
   IEnumerator DelayAction(float delayTime, CallbackFunction Action) {
     // Wait for the specified delay time before continuing.
     yield return new WaitForSeconds(delayTime);
 
     // Do the action after the delay time has finished.
     Action();
+  }
+
+  public void OnButtonClick(GameObject buttonObject) {
+    // event when button is clicked
+    if(state == GameState.STARTING || state == GameState.RESULT) return;
+    yourChoice = int.Parse(buttonObject.name.Substring(0, 1));
+    CheckResult();
+  }
+  public void OnMouseExit(GameObject buttonObject) {
+    // event when mouse exit the button
+
+    // get the animator component from the button
+    Animator anim = buttonObject.GetComponent<Animator>();
+    // switch to idle animation
+    anim.Play("Normal");
   }
 
   public void BackToMenu() { SceneManager.LoadScene("MenuScene"); }
